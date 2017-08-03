88a3d8ab60f14706524f732c8085cb9a3b5011a7
Crhistian Ramirez <cramirez@four51.com>
2017-08-03 11:57:55 -0500

    forgot to build minified files

--- dist/script.js
+++ dist/script.js
@@ -2,57 +2,6 @@ angular.module('bachmans-common', [
 
 ]);
 angular.module('bachmans-common')
-    .config(OrderCloudSDKAnonAdditions)
-;
-
-function OrderCloudSDKAnonAdditions($provide){
-    $provide.decorator('OrderCloudSDK', ['$delegate', '$cookies', 'ocAppName', function($delegate, $cookies, ocAppName){
-        var cookiePrefix = ocAppName.Watch().replace(/ /g, '_').toLowerCase();
-        var anonCookieName = cookiePrefix + 'anonymous.token';
-        var impersonationCookieName = cookiePrefix + 'impersonate.token';
-        
-        // enables use of As(), AsAnon(), and AsAdmin()
-        // so that we can easily tell under which context
-        // an api call is being made
-
-        $delegate.GetAnonToken = function(){
-            return $cookies.get(anonCookieName);
-        };
-
-        $delegate.SetAnonToken = function(token){
-            $cookies.put(anonCookieName, token);
-        };
-
-        $delegate.GetImpersonationToken = function(){
-            return $cookies.get(impersonationCookieName);
-        };
-
-        $delegate.SetImpersonationToken = function(token){
-            $cookies.put(impersonationCookieName, token);
-        };
-
-        var originalAs = $delegate.As;
-
-        $delegate.As = function(){
-            var impersonationToken = $delegate.GetImpersonationToken();
-            return originalAs(impersonationToken);
-        };
-
-        $delegate.AsAnon = function(){
-            var anonymousToken = $delegate.GetAnonToken();
-            return originalAs(anonymousToken);
-        };
-
-        $delegate.AsAdmin = function(){
-            var adminToken = $delegate.GetToken();
-            return originalAs(adminToken);
-        };
-
-        return $delegate;
-    }]);
-}
-OrderCloudSDKAnonAdditions.$inject = ['$provide'];
-angular.module('bachmans-common')
     .config(OrderCloudSDKBuyerXP);
 
 
@@ -87,6 +36,66 @@ function OrderCloudSDKBuyerXP($provide){
     }]);
 }
 OrderCloudSDKBuyerXP.$inject = ['$provide'];
+angular.module('bachmans-common')
+    .config(OrderCloudSDKAnonAdditions)
+;
+
+function OrderCloudSDKAnonAdditions($provide){
+    $provide.decorator('OrderCloudSDK', ['$delegate', '$cookies', 'ocAppName', function($delegate, $cookies, ocAppName){
+        var cookiePrefix = ocAppName.Watch().replace(/ /g, '_').toLowerCase();
+        var anonCookieName = cookiePrefix + 'anonymous.token';
+        var impersonationCookieName = cookiePrefix + 'impersonate.token';
+        var adminCookieName = cookiePrefix + 'admin.token';
+        
+        // enables use of As(), AsAnon(), and AsAdmin()
+        // so that we can easily tell under which context
+        // an api call is being made
+
+        $delegate.GetAnonToken = function(){
+            return $cookies.get(anonCookieName);
+        };
+
+        $delegate.SetAnonToken = function(token){
+            $cookies.put(anonCookieName, token);
+        };
+
+        $delegate.GetImpersonationToken = function(){
+            return $cookies.get(impersonationCookieName);
+        };
+
+        $delegate.SetImpersonationToken = function(token){
+            $cookies.put(impersonationCookieName, token);
+        };
+
+        $delegate.SetAdminToken = function(token){
+            $cookies.put(adminCookieName, token);
+        };
+
+        $delegate.RemoveAdminToken = function(){
+            $cookies.remove(adminCookieName);
+        };
+        
+        var originalAs = $delegate.As;
+
+        $delegate.As = function(){
+            var impersonationToken = $delegate.GetImpersonationToken();
+            return originalAs(impersonationToken);
+        };
+
+        $delegate.AsAnon = function(){
+            var anonymousToken = $delegate.GetAnonToken();
+            return originalAs(anonymousToken);
+        };
+
+        $delegate.AsAdmin = function(){
+            var adminToken = $delegate.GetToken();
+            return originalAs(adminToken);
+        };
+
+        return $delegate;
+    }]);
+}
+OrderCloudSDKAnonAdditions.$inject = ['$provide'];
 
 angular.module('bachmans-common')
     .factory('bachBuyerXp', bachBuyerXpService)
