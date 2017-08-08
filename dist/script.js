e84c96627b3c92e1fe051dd7c8ea8fbe41b90d0f
Crhistian Ramirez <cramirez@four51.com>
2017-08-08 09:32:43 -0500

    add list methods

--- dist/script.js
+++ dist/script.js
@@ -12,6 +12,22 @@ function OrderCloudSDKBuyerXP($provide){
             return bachBuyerXp.Get(token);
         };
 
+        $delegate.Buyers.List = function(){
+            return $delegate.Buyers.Get()
+                .then(function(buyerxp){
+                    return {
+                        Items: [buyerxp], 
+                        Meta: {
+                            Page: 1, 
+                            PageSize: 1, 
+                            TotalPages: 1, 
+                            TotalCount: 1, 
+                            ItemRange: [1, 1]
+                        }
+                    };
+                });
+        };
+
         $delegate.Buyers.Update = function(){
             var update = [].slice.call(arguments)[1]; //update obj is second argument
             var token = $delegate.GetToken();
