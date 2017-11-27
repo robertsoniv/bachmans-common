de353cb5a93f2f389a99d4025e0a289176c40eb5
Alexa Rarick <ararick@four51.com>
2017-11-27 10:53:20 -0600

    add $q defer to return correct data for purple perks

--- src/services/bach-purple-perks.js
+++ src/services/bach-purple-perks.js
@@ -1,41 +1,46 @@
 angular.module('bachmans-common')
 .factory('bachPP', bachmansPurplePerks);
 
-function bachmansPurplePerks($http, $filter) {
+function bachmansPurplePerks($http, $filter, $q) {
 var service = {
     CheckBalance: _checkBalance
 }
 
 function _checkBalance(user) {
     var date = new Date();
+    var defer = $q.defer();
     var expirationDate = new Date(date.getFullYear(), 3 * (Math.ceil((date.getMonth() + 1) / 3)), 1) - 1;     
-    var purplePerks = {};
-    return $http.post('https://Four51TRIAL104401.jitterbit.net/BachmansOnPrem/PurplePerksBalanceCheck', {
+    $http.post('https://Four51TRIAL104401.jitterbit.net/BachmansOnPrem/PurplePerksBalanceCheck', {
         "card_number": "777777" + user.xp.LoyaltyID
-        }).success(function(perks) {
-            if (perks.card_value != "cardNumber not available" && perks.card_value > 0) {
-                purplePerks = {
-                    Balance: Number(perks.card_value),
-                    PointsEarned: perks.card_value,
-                    CardNumber: "777777" + user.xp.LoyaltyID,
-                    LoyaltyID: user.xp.LoyaltyID,
-                    ExpirationDate: $filter('date')(expirationDate, 'MM/dd/yyyy')
+        }).then(function(perks) {
+                var purplePerks = {};
+                if (perks.data && perks.data.card_value != "cardNumber not available" && perks.data.card_value > 0) {
+                    purplePerks = {
+                        Balance: Number(perks.data.card_value),
+                        PointsEarned: perks.data.card_value,
+                        CardNumber: "777777" + user.xp.LoyaltyID,
+                        LoyaltyID: user.xp.LoyaltyID,
+                        ExpirationDate: $filter('date')(expirationDate, 'MM/dd/yyyy')
+                    }
+                    defer.resolve(purplePerks);
+                } else {
+                    purplePerks = {
+                        Balance: 0,
+                        PointsEarned: 0,
+                        CardNumber: "777777" + user.xp.LoyaltyID,
+                        LoyaltyID: user.xp.LoyaltyID,
+                        ExpirationDate: $filter('date')(expirationDate, 'MM/dd/yyyy')
+                    }
+                    defer.resolve(purplePerks);           
                 }
-            } else {
-                purplePerks = {
-                    Balance: 50,
-                    PointsEarned: 0,
-                    CardNumber: "777777" + user.xp.LoyaltyID,
-                    LoyaltyID: user.xp.LoyaltyID,
-                    ExpirationDate: $filter('date')(expirationDate, 'MM/dd/yyyy')
-                }
-            }
-            return purplePerks;
-        }).error(function(error) {
-            console.log(error);
-            return null;
-            
-        });
+                
+            })
+            .catch(function(error) {
+                console.log(error);
+                defer.reject(error);
+                
+            });
+        return defer.promise;
     }
     return service;
 }
