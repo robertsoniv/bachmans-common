5ab2b3fd3ba0925c34e58917d8408fcfd35ad103
Robert Watt <rwatt@four51.com>
2018-10-29 10:45:43 -0500

    Update gift card list request

--- src/services/bach-gift-cards.js
+++ src/services/bach-gift-cards.js
@@ -1,8 +1,7 @@
 angular.module('bachmans-common')
-    .factory('bachGiftCards', bachGiftCards)
-;
+    .factory('bachGiftCards', bachGiftCards);
 
-function bachGiftCards(nodeapiurl, $resource, toastr, $http, OrderCloudSDK){
+function bachGiftCards(nodeapiurl, $resource, toastr, $http, OrderCloudSDK) {
     var service = {
         Create: _create,
         Update: _update,
@@ -12,42 +11,62 @@ function bachGiftCards(nodeapiurl, $resource, toastr, $http, OrderCloudSDK){
         Release: _release
     };
 
-    function _create(req){
+    function _create(req) {
         return GiftCards().create(req).$promise;
     }
 
-    function _update(req){
-        if(!req.body && !req.body.id) return toastr.error('id is a required parameter');
-        return GiftCards().update({id: req.body.id}, req.body).$promise;
+    function _update(req) {
+        if (!req.body && !req.body.id) return toastr.error('id is a required parameter');
+        return GiftCards().update({
+            id: req.body.id
+        }, req.body).$promise;
     }
 
-    function _delete(req){
-        return GiftCards().delete({id: req.id}).$promise;
+    function _delete(req) {
+        return GiftCards().delete({
+            id: req.id
+        }).$promise;
     }
 
-    function _list(req){
-        return GiftCards().list(req && req.searchTerm ? {searchTerm: req.searchTerm} : null).$promise
-            .then(function(results){
+    function _list(req) {
+        return GiftCards().list(req).$promise
+            .then(function (results) {
                 return results.list;
             });
     }
 
-    function _purchase(req){
-        return $http.post(nodeapiurl + '/giftcards/purchase/' + req.orderid, {}, {headers: {'oc-token': OrderCloudSDK.GetToken()}});
+    function _purchase(req) {
+        return $http.post(nodeapiurl + '/giftcards/purchase/' + req.orderid, {}, {
+            headers: {
+                'oc-token': OrderCloudSDK.GetToken()
+            }
+        });
     }
 
     function _release(req) {
-        return $http.post(nodeapiurl + '/giftcards/release/' + req.orderid, {}, {headers: {'oc-token': OrderCloudSDK.GetToken()}});
+        return $http.post(nodeapiurl + '/giftcards/release/' + req.orderid, {}, {
+            headers: {
+                'oc-token': OrderCloudSDK.GetToken()
+            }
+        });
     }
-    
-    function GiftCards(){
+
+    function GiftCards() {
         var methods = {
-            create: {method: 'POST'},
-            update: {method: 'PUT'},
-            delete: {method: 'DELETE'},
-            list: {method: 'GET'}
+            create: {
+                method: 'POST'
+            },
+            update: {
+                method: 'PUT'
+            },
+            delete: {
+                method: 'DELETE'
+            },
+            list: {
+                method: 'GET'
+            }
         };
-        _.each(methods, function(method){
+        _.each(methods, function (method) {
             method.headers = {
                 'oc-token': OrderCloudSDK.GetToken()
             };
